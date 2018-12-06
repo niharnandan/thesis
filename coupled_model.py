cbf22fd26353370965ea8f377650a7ff458af187
niharnandan <nihar.nandan@gmail.com>
2018-12-05 23:32:21 -0700

    Added likelihood for DOECLIM

--- coupled_model.py
+++ coupled_model.py
@@ -32,7 +32,7 @@ dfTemperature = pd.read_csv('https://media.githubusercontent.com/media/MUSSLES/t
 sealevel = (dfSealevel.loc[(dfSealevel["year"]>=1880) & (dfSealevel["year"]<=2009), "sealevel"]).tolist()
 year = (dfSealevel.loc[(dfSealevel["year"] >= 1880) & (dfSealevel["year"]<=2009), "year"]).tolist()
 sealevel_sigma = (dfSealevel.loc[(dfSealevel["year"]>=1880) & (dfSealevel["year"]<=2009), "uncertainty"]).tolist()
-#temperatures = dfTemperature.loc[(dfTemperature["Time"] <= 2009) & (dfTemperature["Time"] >= 1880), "Historical NOAA temp & CNRM RCP 8.5 with respect to 20th century"]
+dfTemperature = dfTemperature.loc[(dfTemperature["Time"] <= 2008) & (dfTemperature["Time"] >= 1880), "Historical NOAA temp & CNRM RCP 8.5 with respect to 20th century"]
 forcing = pd.read_csv( 'data/forcing_hindcast.csv')
 mod_time = forcing['year']
 
@@ -47,45 +47,61 @@ def update_cov(X, s_d, size):
     return s_d*cov + I_d*eps*s_d
 #Online update 
 
-def prior(theta, sealevel_0, unc_0):
+def prior(theta, sealevel_0, unc_0, pamnames):
 	log_prior = 0
 	#unpacking the variables individually for clarity
+
 	alpha, Teq, S0, rho, sigma_ar = (
 		theta[0], theta[1], theta[2], theta[3], theta[4])
-	if (len(theta) >= 1):
+
+	if ('alpha' in pamnames):
 		log_prior += stats.uniform.logpdf(alpha, loc = 0, scale = 5) #lb and ub?
-	if (len(theta) >=  2):
+	if ('Teq' in pamnames):
 		log_prior += stats.uniform.logpdf(Teq, loc=-1, scale = 2)
-	if (len(theta) >=  3):
+	if ('S0' in pamnames):
 		log_prior += stats.norm.logpdf(S0, loc = sealevel_0, scale = unc_0)
-	if (len(theta) >=  4):
+	if ('rho' in pamnames):
 		log_prior += stats.uniform.logpdf(rho, loc = 0, scale = 1)
-	if (len(theta) >=  5):
+	if ('sigma_ar' in pamnames):
 		log_prior += stats.uniform.logpdf(sigma_ar, loc = 0, scale = 5)
-	if (len(theta) >=  6):
+	if ('climate_sensitivity' in pamnames):
 		log_prior += stats.uniform.logpdf(theta[5], loc = 0.1, scale = 9.9)
-	if (len(theta) >=  7):
+	if ('ocean_vertical_diffusivity' in pamnames):
 		log_prior += stats.uniform.logpdf(theta[6], loc = 0.1, scale = 3.9)
-	if (len(theta) >=  8):
+	if ('aerosol_scaling' in pamnames):
 		log_prior += stats.uniform.logpdf(theta[7], loc = 0, scale = 2)
-	if (len(theta) >=  9):
+	if ('T_0' in pamnames):
 		log_prior += stats.uniform.logpdf(theta[8], loc = -0.3, scale = 0.6)
+	if ('sigma_T' in pamnames):
+		log_prior += stats.uniform.logpdf(theta[9], loc = 0.05, scale = 5.05)
+	if ('rho_T' in pamnames):
+		log_prior += stats.uniform.logpdf(theta[10], loc =  0, scale = 0.999)
 	return log_prior
 
-def logp(theta, sealevel, deltat, temperatures, model, sigma=sealevel_sigma):
-    N = len(sealevel)
-    alpha, Teq, S0, rho, sigma_ar = (
-    theta[0], theta[1], theta[2], theta[3], theta[4])
-    resid = np.array([sealevel[i] - model[i] for i in range(len(model))])
-    sigma_obs = np.diag([i**2 for i in sigma])
-    sigma_ar1 = build_ar1(rho, sigma_ar, N)
-    log_prior = prior(theta, sealevel[0], sealevel_sigma[0])
-    if np.isinf(log_prior): return -np.inf
-    cov = np.add(sigma_obs,sigma_ar1)
-    cov = np.multiply((np.transpose(cov) + cov), 1/2)
-    log_likelihood = stats.multivariate_normal.logpdf(resid, cov=cov)
-    log_posterior = log_likelihood + log_prior
-    return log_posterior
+def logp(theta, sealevel, deltat, temperatures, model, pamnames, sigma=sealevel_sigma):
+	N = len(sealevel)
+	alpha, Teq, S0, rho, sigma_ar = (
+	theta[0], theta[1], theta[2], theta[3], theta[4])
+	resid = np.array([sealevel[i] - model[i] for i in range(len(model))])
+
+	sigma_obs = np.diag([i**2 for i in sigma])
+	sigma_ar1 = build_ar1(rho, sigma_ar, N) if ('sigma_ar' in pamnames) else []
+
+	t_residual = dfTemperature - temperatures
+
+	sigma_ar1_T = build_ar1(theta[9], theta[9], N) if ('sigma_T' in pamnames) else []
+
+	log_prior = prior(theta, sealevel[0], sealevel_sigma[0], pamnames)
+	if np.isinf(log_prior): return -np.inf
+	
+	cov = np.add(sigma_obs,sigma_ar1)
+	cov = np.multiply((np.transpose(cov) + cov), 1/2)
+	
+	log_likelihood = stats.multivariate_normal.logpdf(resid, cov=cov)
+	
+	log_likelihood_T = stats.multivariate_normal.logpdf(t_residual, cov=sigma_ar1_T)
+	log_posterior = log_likelihood + log_prior
+	return log_posterior
 
 def build_ar1(rho, sigma_ar, length):
     ar1 = []
@@ -118,83 +134,83 @@ def online_update_cov(X, m, Ct, Sd, eps=0.0001):
     cov = part1 + (Sd/t)*(part2 - part3 + part4 + part5)
     return (cov + np.transpose(cov))/2, m1
 
-def coupled_model(forcingtotal, doeclim_out, temperatures, model, parameters, mod_time, T_0):
-    forcingtotal = forcing_total.forcing_total(forcing=forcing, alpha_doeclim=parameters[7], l_project=False, begyear=mod_time[0], endyear=np.max(mod_time))
-    doeclim_out = doeclimF.doeclimF(forcingtotal, mod_time, S=parameters[5], kappa=parameters[6])
-    temperatures = np.array((doeclim_out.loc[(doeclim_out["time"]>=1880) & (doeclim_out["time"]<=2008), "temp"]).tolist())
-    temperatures = temperatures + T_0
-    model = gmsl_model.gmsl_model(parameters, temperatures, deltat)
+def coupled_model(forcingtotal, doeclim_out, temperatures, model, parameters, mod_time, T_0, pamnames):
+	if 'aerosol_scaling' in pamnames:
+		forcingtotal = forcing_total.forcing_total(forcing=forcing, alpha_doeclim=parameters[7], l_project=False, begyear=mod_time[0], endyear=np.max(mod_time))
+	if 'climate_sensitivity' in pamnames and 'ocean_vertical_diffusivity' in pamnames:
+		doeclim_out = doeclimF.doeclimF(forcingtotal, mod_time, S=parameters[5], kappa=parameters[6])
+	temperatures = np.array((doeclim_out.loc[(doeclim_out["time"]>=1880) & (doeclim_out["time"]<=2008), "temp"]).tolist())
+	temperatures = temperatures + T_0
+	model = gmsl_model.gmsl_model(parameters, temperatures, deltat)
 
-def chain(parameters, temperatures, deltat, sealevel, sealevel_sigma, N=10000):
-    
-    forcingtotal = forcing_total.forcing_total(forcing=forcing, alpha_doeclim=aerosol_scaling, l_project=False, begyear=mod_time[0], endyear=np.max(mod_time))
-    doeclim_out = doeclimF.doeclimF(forcingtotal, mod_time, S=climate_sensitivity, kappa=ocean_vertical_diffusivity)
-    temperatures = np.array((doeclim_out.loc[(doeclim_out["time"]>=1880) & (doeclim_out["time"]<=2008), "temp"]).tolist())
-    temperatures = temperatures + T_0
-    alpha, Teq, S0 = parameters[0], parameters[1], parameters[2]
-    theta = parameters
-    print('Initial estimate for parameters -', theta)
-    
-    model = gmsl_model.gmsl_model(theta, temperatures, deltat)
+def chain(parameters, temperatures, deltat, sealevel, sealevel_sigma, pamnames, N=10000):
+	if 'aerosol_scaling' in pamnames:
+		forcingtotal = forcing_total.forcing_total(forcing=forcing, alpha_doeclim=aerosol_scaling, l_project=False, begyear=mod_time[0], endyear=np.max(mod_time))
+	if 'climate_sensitivity' in pamnames and 'ocean_vertical_diffusivity' in pamnames:
+		doeclim_out = doeclimF.doeclimF(forcingtotal, mod_time, S=climate_sensitivity, kappa=ocean_vertical_diffusivity)
+	temperatures = np.array((doeclim_out.loc[(doeclim_out["time"]>=1880) & (doeclim_out["time"]<=2008), "temp"]).tolist())
+	temperatures = temperatures + T_0
+	theta = parameters
+	print('Initial estimate for parameters -', theta)
 
-    lp = logp(theta, sealevel, deltat, temperatures, model, sigma=sealevel_sigma)
-    theta_best = theta
-    lp_max = lp
-    theta_new = [0.] * len(parameters)
-    accepts = 0
-    mcmc_chains = np.array([np.zeros(len(parameters))] * N)
-    stepsizes = [.01, .05, 5, .001, .1, 0.16, 0.17, 0.025, 0.03]
-    step = []
-    for i in range(len(parameters)):
-        temp = [0]*len(parameters)
-        temp[i] = stepsizes[i]
-        step.append(temp)
-    step = np.array(step)
-    #step = np.array([[.01,0,0,0,0,0,0,0,0], [0,.05,0,0,0,0,0,0,0], [0,0,5,0,0,0,0,0,0], [0,0,0,0.001,0,0,0,0,0],[0,0,0,0,0.1,0,0,0,0],
+	model = gmsl_model.gmsl_model(theta, temperatures, deltat)
+
+	lp = logp(theta, sealevel, deltat, temperatures, model, pamnames, sigma=sealevel_sigma)
+	theta_best = theta
+	lp_max = lp
+	theta_new = [0.] * len(parameters)
+	accepts = 0
+	mcmc_chains = np.array([np.zeros(len(parameters))] * N)
+	stepsizes = {'alpha': .01, 'Teq': .05, 'S0': 5, 'rho': .001, 'sigma_ar': .1, 'climate_sensitivity': 0.16, 'ocean_vertical_diffusivity': 0.17, 'aerosol_scaling': 0.025, 'T_0': 0.03, 'sigma_T': 5e-4, 'rho_T': 0.007}
+	step = []
+	count = 0
+	for i in pamnames:
+		temp = [0]*len(pamnames)
+		temp[count] = stepsizes[i]
+		count += 1
+		step.append(temp)
+	step = np.array(step)
+	#step = np.array([[.01,0,0,0,0,0,0,0,0], [0,.05,0,0,0,0,0,0,0], [0,0,5,0,0,0,0,0,0], [0,0,0,0.001,0,0,0,0,0],[0,0,0,0,0.1,0,0,0,0],
 	#				 [0,0,0,0,0,0.16,0,0,0], [0,0,0,0,0,0,0.17,0,0], [0,0,0,0,0,0,0,0.025,0],[0,0,0,0,0,0,0,0,0.03]])
-    sd = 2.38**2 / len(theta)
+	sd = 2.38**2 / len(theta)
 	#Check if converged. If not keep running. 
-    print(N)
-    for i in range(N):
-        if i > 500: step = update_cov(mcmc_chains[:i], sd, len(parameters))
-        theta_new = list(np.random.multivariate_normal(theta, step))
-        coupled_model(forcingtotal, doeclim_out, temperatures, model, parameters, mod_time, T_0)
+	print(N)
+	for i in range(N):
+		if i > 500: step = update_cov(mcmc_chains[:i], sd, len(parameters))
+		theta_new = list(np.random.multivariate_normal(theta, step))
+		coupled_model(forcingtotal, doeclim_out, temperatures, model, parameters, mod_time, T_0, pamnames)
 		#Separate above 3 lines + T_0 into a new file. 
-        lp_new = logp(theta_new, sealevel, deltat, temperatures, model, sigma=sealevel_sigma)
-        lq = lp_new - lp
-        lr = np.math.log(np.random.uniform(0, 1))
-        #print(lr, lq)
-        if (lr < lq):
-            theta = theta_new
-            lp = lp_new
-            accepts += 1
-            if lp > lp_max:
-                theta_best = theta
-                lp_max = lp
-        mcmc_chains[i] = theta
+		lp_new = logp(theta_new, sealevel, deltat, temperatures, model, pamnames, sigma=sealevel_sigma, )
+		lq = lp_new - lp
+		
+		lr = np.math.log(np.random.uniform(0, 1))
+		#print(lr, lq)
+		if (lr < lq):
+			theta = theta_new
+			lp = lp_new
+			accepts += 1
+			if lp > lp_max:
+				theta_best = theta
+				lp_max = lp
+		mcmc_chains[i] = theta
 
-    return mcmc_chains,accepts/N*100
+	return mcmc_chains,accepts/N*100
 
-parameters = [3.4, -0.5, sealevel[0], 0.5, 3, climate_sensitivity, ocean_vertical_diffusivity, aerosol_scaling, T_0]
+pamnames = ['alpha', 'Teq', 'S0', 'rho', 'sigma_ar', 'climate_sensitivity', 'ocean_vertical_diffusivity', 'aerosol_scaling', 'T_0', 'sigma_T', 'rho_T']
+parameters = [3.4, -0.5, sealevel[0], 0.5, 3, climate_sensitivity, ocean_vertical_diffusivity, aerosol_scaling, T_0, 0.1, 0.55]
 deltat = 1
-mcmc_chain,accept_rate = chain(parameters, temperatures, deltat, sealevel, sealevel_sigma, N=NUMBER)
+mcmc_chain,accept_rate = chain(parameters, temperatures, deltat, sealevel, sealevel_sigma, pamnames, N=NUMBER)
 
 for i in range(len(parameters)):
 	fig, ax = plt.subplots(nrows=1, ncols=1 )  # create figure & 1 axis
 	ax.plot(mcmc_chain[:,i])
+	ax.set_title(pamnames[i])
 	fig.savefig('image/plot'+str(i+1)+'.png')   # save the figure to file
 	plt.close(fig)
 
-for i in range(len(parameters)):
+for i in range(len(pamnames)):
 	fig, ax = plt.subplots(nrows=1, ncols=1 )  # create figure & 1 axis
 	ax.hist(mcmc_chain[:,i], density = True, facecolor='green', alpha=0.5, bins = 20, edgecolor = 'white')
+	ax.set_title(pamnames[i])
 	fig.savefig('image/hist'+str(i+1)+'.png')   # save the figure to file
 	plt.close(fig)
-	
-'''TO-DO
-	Switch for parameters - DONE
-	Make histogram for each parameter (USE SUPERCOMPUTER)
-	Compare to paper sent from Tony
-	Use PYMC3 package.
-	Multiprocessing for Python 
-'''
